751a8bf9029de3c621bfd6db27bb14b01ce1d71b
Scott Gibson <scottchgibson@gmail.com>
2026-09-09 17:41:40 +0200

    Cleanup plot code slightly

--- modules/plot.py
+++ modules/plot.py
@@ -2,13 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colorizer as mcolorizer
 import matplotlib.colors as mcolors
+import matplotlib.animation as animation
 
 def create_dummy_array(n: int):
     """
     Dummy function, simply creates a mock output of an n x n grid, with random temperature
-    data in cells ranging from 0 to 100.
+    data in cells ranging from 20 to 80.
     """
-    return 100 * np.random.random((n, n))
+    return 20 + 80 * np.random.random((n, n))
+
+def cleanup_ax(ax):
+    """
+    Cleans up the axes by removing tick marks.
+    """
+    ax.tick_params(axis='both', which='both', bottom=False, left=False, labelbottom = False, labelleft = False)
 
 def plot_temperature(room_states):
     """
@@ -16,6 +23,7 @@ def plot_temperature(room_states):
     Namely, the initial room state, an animation switching between room states, and a final
     room state.
 
+    # TODO: Determine how to stitch together three separate rooms into a single graph object.
     # TODO: Certain theming and timing vars should be moved to parameters with reasonable defaults.
     # TODO: It should maybe be possible to opt in/out to the animation, since that looping forever could
             be distracting when trying to talk through the output. Or, the speed of the animation could be
@@ -30,7 +38,6 @@ def plot_temperature(room_states):
     # Configure subplots
     fig, axes = plt.subplots(1, 3)
     fig.suptitle("Heat Flow Visualized")
-    # im = ax.imshow(create_dummy_array(), cmap="BlRe")
 
     # Create a colorizer with a predefined norm to be shared across all images
     norm = mcolors.Normalize(vmin=0, vmax=100)
@@ -40,20 +47,11 @@ def plot_temperature(room_states):
     # Attach heatmap to each of the three displays.
     images = []
     for ax, data, title in zip(axes.flat, [room_states[0], room_states[0], room_states[-1]], ["Initial State", "Animation", "Final State"]):
+        # Update subplot title
         ax.set_title(title)
-        # Remove ticks and labels
-        ax.tick_params(
-            axis='both',
-            which='both',
-            bottom=False,
-            top=False,
-            left=False,
-            right=False,
-            labelbottom = False,
-            labeltop = False,
-            labelleft = False,
-            labelright = False
-        )
+        # Clean subplot details
+        cleanup_ax(ax)
+        # Draw heatmap
         images.append(ax.imshow(data, colorizer=colorizer))
 
     # Display shared colorbar
