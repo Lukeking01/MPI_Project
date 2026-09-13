65796751a23da4cd496b1118d029cfc61bc8b10f
Lukas Nord <Lukassigvard@gmail.com>
2026-09-13 11:28:26 +0200

    bug fixes

--- modules/dn_MPI.py
+++ modules/dn_MPI.py
@@ -7,64 +7,34 @@
 #        ↓
 #repeat
 import numpy as np
-from geometry import get_interfaces
-from iteration import solve_room1, solve_room2, solve_room3
-from MPI import send_npdata, recv_npdata
+from .geometry import get_interfaces
+from .iteration import solve_room1, solve_room2, solve_room3
+from .MPI import send_npdata, recv_npdata
 
 def dn_iteration(room, dx, rank):
 
     
     nx = room.shape[1] - 2
     ny = room.shape[0] - 2
-    
-
-    # -----------------------------------------
-    # 1. Update Room 2's interface temperatures
-    # -----------------------------------------
-    
-    ## TODO
-    ## This needs to be altered using MPI
-    room2, _, _ = get_interfaces(
-        room1,
-        room2,
-        room3,
-        dx
-    )
-    
 
     if rank == 1:
-        room = room2
+        # -----------------------------------------
+        # 1. Update Room 2's interface temperatures and send fluxes
+        # -----------------------------------------
+        room = get_interfaces(room,dx)
         # -----------------------------------------
         # 2. Solve Room 2 with Dirichlet conditions
         # -----------------------------------------
 
         room = solve_room2(room)
 
-        # -----------------------------------------
-        # 3. Calculate fluxes from Room 2
-        # -----------------------------------------
-
-        flux1 = -1 * (
-            room[1:nx+1, 1] -
-            room[1:ny+1, 0]
-        ) / dx
-        
-        send_npdata(flux1,0)
-        
-        flux3 = -1 * (
-            room[nx+2:-1, -1] -
-            room[ny+2:-1, -2]
-        ) / dx
-
-        send_npdata(flux3,2)
         return room
         
     if rank == 0:
         # -----------------------------------------
         # 4. Solve Room 1 with Neumann
         # -----------------------------------------
-        flux1 = np.empty(ny)
-        recv_npdata(flux1,1)
+        flux1 = get_interfaces(room,dx)
         
         room1 = solve_room1(
             room,
@@ -79,8 +49,7 @@ def dn_iteration(room, dx, rank):
         # -----------------------------------------
         # 5. Solve Room 3 with Neumann
         # -----------------------------------------
-        flux3 = np.empty(ny)
-        recv_npdata(flux3,1)
+        flux3 = get_interfaces(room,dx)
         room3 = solve_room3(
             room,
             flux3,
@@ -116,7 +85,7 @@ def dirichlet_neumann(room, dx, rank, iterations=10, omega=0.8):
             room.copy()
         )
 
-    return states
+    return np.array(states)
 
 def relax(u_new, u_old, omega):
     """
