bdb8b2ea1c89e5168287cae3d72133f483cd47a3
Scott Gibson <scottchgibson@gmail.com>
2026-09-16 10:59:30 +0200

    Add custom plot params dict to constants file

--- modules/constants.py
+++ modules/constants.py
@@ -1,7 +1,7 @@
 ### Room Scenario Params ###
 
 # Amount of subdivisions to divide a square room into.
-DX = 1 / 200
+DX = 1 / 40
 # Smoothing param, lower to reduce large changes in temperature state between iterations.
 OMEGA = 0.8 
 # Number of iterations to run.
@@ -21,4 +21,30 @@ FLOOR_TEMP = 0.0    # Generic starting room temperature, changes through iterati
 # Set to true to save all iterations and animate from the initial state to the end state.
 ANIMATE = True
 # Set to True to crop out the boundary conditions, so they won't show in the plot.
-CROP = False
+CROP = True
+
+# Keyword args to configure the plotting functionality, should be compatible with
+# `plot_temperature()`
+PLOT_PARAMS = { # Default params
+    "cmap": "inferno", # Chosen color map scheme
+    "norm_min": 0, # Normalizes 0 to be the coldest value
+    "norm_max": 40, # Normalizes 40 to be the hottest value
+}
+
+# Visual change to heat spreading through water
+PLOT_PARAMS_OCEAN = {
+    "cmap": "ocean",
+    "norm_min": -15,
+    "norm_max": 50,
+}
+
+# Visual change to heat leaving water
+PLOT_PARAMS_ICE = {
+    "cmap": "ocean",
+    "norm_min": -80,
+    "norm_max": 40,
+}
+
+# Keyword args to configure the animation functionality, should be compatible with
+# `matplotlib.animation.FuncAnimation()`
+
